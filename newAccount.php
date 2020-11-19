e1fe8a52cc7bfe418d3e4c456b2bf7beee87ccd7
Kyle Cameron <Kcam99@outlook.com>
2020-11-19 14:47:15 -0500

    Update newAccount.php

--- newAccount.php
+++ newAccount.php
@@ -12,11 +12,11 @@
     $password = $_POST['password'] ?? '';
     $user = $_POST['username'] ?? '';
 
-
-    $query1="INSERT INTO profile (streetAddr, city, state, zip) VALUES ( '$address', '$city', '$state', '$zip')";
-    $db->exec($query1);
     $query="INSERT INTO user (firstName, lastName, username, email, password) VALUES ('$first', '$last', '$user', '$email', '$password')";
     $db->exec($query);
+    $query1="INSERT INTO profile (user_id, streetAddr, city, state, zip) VALUES ('$user_id', '$address', '$city', '$state', '$zip')";
+    $db->exec($query1);
+    
     
     include('index.php');
 ?>
