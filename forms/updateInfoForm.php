debd8380fc3bef18b3be928bc8d772b0c33beb77
Caroline Umila <cmu12314@uga.edu>
2020-11-22 17:45:20 -0500

    fixed bug where full address didn't display

--- forms/updateInfoForm.php
+++ forms/updateInfoForm.php
@@ -3,22 +3,22 @@
                     <legend><b>Personal Information</b></legend>
                     
                     <label for="firstName">First Name:</label>
-                    <input type="text" name="firstName" placeholder="First Name" value = <?php echo $data['firstName']; ?> required><br>
+                    <input type="text" name="firstName" placeholder="First Name" value = "<?php echo $data['firstName']; ?>" required><br>
 
                     <label for="lastName">Last Name:</label>
-                    <input type="text" name="lastName" placeholder="Last Name" value = <?php echo $data['lastName']; ?> required><br><br>
+                    <input type="text" name="lastName" placeholder="Last Name" value = "<?php echo $data['lastName']; ?>" required><br><br>
 
                     <label for="streetAddress">Street Address:</label>
-                    <input type="text" name="streetAddress" placeholder="123 Dawg St." value = <?php echo $profile['streetAddr']; ?> required><br>
+                    <input type="text" name="streetAddress" placeholder="123 Dawg St." value = "<?php echo $profile['streetAddr']; ?>" required><br>
 
                     <label for="city">City:</label>
-                    <input type="text" name="city" placeholder="Athens" value = <?php echo $profile['city']; ?> required><br>
+                    <input type="text" name="city" placeholder="Athens" value = "<?php echo $profile['city']; ?>" required><br>
 
                     <label for="state">State:</label>
-                    <input type="text" name="state" placeholder="Georgia" value = <?php echo $profile['state']; ?> required><br>
+                    <input type="text" name="state" placeholder="Georgia" value = "<?php echo $profile['state']; ?>" required><br>
 
                     <label for="zipCode">Zip Code:</label>
-                    <input type="text" name="zipCode" placeholder="30606" value = <?php echo $profile['zip']; ?> required><br>
+                    <input type="text" name="zipCode" placeholder="30606" value = "<?php echo $profile['zip']; ?>" required><br>
 
                 </fieldset> <br>
 
@@ -26,10 +26,10 @@
                     <legend><b>Account Information</b></legend>
 
                     <label for="username">Username:</label>
-                    <input type="text" name="username" placeholder="hairydawg123" value = <?php echo $data['username']; ?> disabled><br>
+                    <input type="text" name="username" placeholder="hairydawg123" value = "<?php echo $data['username']; ?>" disabled><br>
 
                     <label for="email">Email Address:</label>
-                    <input type="text" name="email" placeholder="hairydawg123@uga.edu" value = <?php echo $data['email']; ?> required><br>
+                    <input type="text" name="email" placeholder="hairydawg123@uga.edu" value = "<?php echo $data['email']; ?>" required><br>
 
                 </fieldset><br>
 
