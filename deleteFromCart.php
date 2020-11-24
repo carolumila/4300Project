d4c4e4fa5b3baaa79c62b75f409be0adea0fdf22
Kyle Cameron <Kcam99@outlook.com>
2020-11-24 10:38:50 -0500

    Fixed duplicate cart removal

--- deleteFromCart.php
+++ deleteFromCart.php
@@ -1,7 +1,7 @@
 <?php
     include('database.php');
     $productID=$_POST['product_id'];
-    $query="DELETE FROM contains WHERE product_id='$productID'";
+    $query="DELETE FROM contains WHERE product_id='$productID' LIMIT 1";
     $db->exec($query);
     include('shopping_cart.php');
 ?>
